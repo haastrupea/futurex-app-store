268f7345f5ff4019c892510630419851003d3323
nwhator <74368087+nwhator@users.noreply.github.com>
2020-11-25 09:14:16 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      isLoading: false
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://fakestoreapi.com/products')
+    .then(res=>res.json())
+    .then(json=>console.log(json))
+    .then(json=> {
+      this.setState({
+        isLoading: true,
+        items: json,
+      })
+    })
+  }
+
+  render() {
+
+    var {isLoading, items} = this.state;
+
+    if (!isLoading) {
+      return <div>Loading...</div>
+    }
+ 
+    else {
+      
+      return (
+        <div className="App">
+          Data has been loaded
+        </div>
+      );
+    }
+
+  } 
+
+
+
 }
 
 export default App;
